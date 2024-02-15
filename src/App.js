c7708cc6205444060c9ebd142dca9b7b58a5cfab
Azhar <kazhikarimova@inbox.ru>
2024-02-15 18:51:20 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [form, setForm] = useState({ name:'', surname:'' });
+  const [forms, setForms] = useState([]);
+
+
+  const addToForm = () => {
+    setForms([...forms, {name: form.name, surname: form.surname}]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='container'>
+    <input type='text' value={form.name} placeholder='Имя'/>
+    <input type='text'value={form.surname} placeholder='Фамилия'/>
+    <button onClick={addToForm()}>Сохранить</button>
+
+    <div>
+    {
+        forms.map((item, index) => (
+        <div key={index}>{item. name} {item.surname}</div>    
+      ))
+    }
     </div>
+    </div>
+    </>
   );
 }
 
